2a494321bd797e8e1f33098a546f27465a0f8cc0
DESKTOP-OU7A1E2\Wei Wang <wei@vibrantsci.com>
2019-01-11 16:39:57 -0800

    first version

---
+++ src/wheatzoomer/NewMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wheatzoomer;
+
+/**
+ *
+ * @author Wei Wang
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(3.2158 - (int)(3.999558));
+    }
+    
+}
